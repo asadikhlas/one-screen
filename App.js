eb1dbb0fc42a4d7fcfdf803337f0d01bccc07103
umairx97 <umair.bajwa@hotmail.com>
2019-05-23 17:48:40 +0500

    completed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, WebView, Dimensions, Image } from 'react-native'
+import { StyleSheet, Text, View, WebView, Dimensions, Image, Vibration } from 'react-native'
 import { Container, Header, Content, Card, CardItem, Footer, FooterTab, Button, Icon, Body } from 'native-base';
 import { Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
@@ -14,14 +14,6 @@ export default class App extends Component {
 
   }
 
-  // Later on in your component
-  async componentDidMount() {
-    await Font.loadAsync({
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      ...Ionicons.font,
-    });
-  }
 
 
   changeId = id => {
@@ -30,94 +22,120 @@ export default class App extends Component {
     })
   }
 
-  showVideo = () => {
+  showVideo = (duration) => {
     const { currentId } = this.state;
+    Vibration.vibrate(duration);
+    Vibration.cancel();
 
     switch (currentId) {
       case 1:
-        return "https://www.youtube.com/embed/MgaKDY1M4Pc"
+        return "https://www.youtube.com/embed/a3ICNMQW7Ok"
 
       case 2:
-        return "https://www.youtube.com/embed/Aznb7eemR_s"
+        return "https://www.youtube.com/embed/xcJtL7QggTI"
+
+      case 3:
+        return "https://www.youtube.com/embed/Bey4XXJAqS8"
+
+      case 4:
+        return "https://www.youtube.com/embed/S-thTTqefls"
+
+      case 5:
+        return "https://www.youtube.com/embed/E5ln4uR4TwQ"
+
+      case 6:
+        return "https://www.youtube.com/embed/tO01J-M3g0U"
+
+      case 7:
+        return "https://www.youtube.com/embed/K1_k03Gvd5g"
+
+      case 9:
+        return "https://www.youtube.com/embed/pSA0x4JIUJs"
+
+      case 10:
+        return "https://www.youtube.com/embed/S-thTTqefls"
 
       default:
-        return "https://www.youtube.com/embed/MgaKDY1M4Pc"
+        return "https://www.youtube.com/embed/Bey4XXJAqS8"
     }
   }
 
   render() {
-    console.log(this.state.currentId)
+
 
     const BUTTONS1 = [
       {
-        name: 'Beauty',
-        iconName: 'apps',
+        name: 'Auto Beauty',
+        url: 'https://image.flaticon.com/icons/png/512/38/38964.png',
         id: 1
       },
 
       {
-        name: 'Camera',
-        iconName: 'camera',
+        name: 'LIquify',
+        url: 'http://www.sclance.com/pngs/cursor-icon-png/cursor_icon_png_356270.png',
         id: 2
       },
 
 
       {
-        name: 'Navigate',
-        iconName: 'navigate',
+        name: 'Facelift',
+        url: 'https://cdn1.iconfinder.com/data/icons/surgery/450/facelift-512.png',
         id: 3
       },
 
       {
-        name: 'Contact',
-        iconName: 'person',
+        name: 'Bigger Eyes',
+        url: 'https://cdn2.iconfinder.com/data/icons/emoticons-hand-drawn/80/Big_Mischievous_Eyes-512.png',
+
         id: 4
       },
       {
-        name: 'ETC',
-        iconName: 'arrow-dropleft',
+        name: 'Dark Circles',
+        url: 'https://cdn2.iconfinder.com/data/icons/natural-beauty/100/dark_circles-512.png',
         id: 5
       },
     ]
 
-
     const BUTTONS2 = [
       {
-        name: 'Apps',
-        url: 'https://img.icons8.com/material/4ac144/256/twitter.png',
+        name: 'Smile',
+        url: 'http://simpleicon.com/wp-content/uploads/smile.png',
         id: 6
       },
 
       {
-        name: 'Camera',
-        url: './assets/unt.png',
+        name: 'Softening',
+        url: 'https://www.pngrepo.com/png/75742/170/emoticon-face-with-the-mouth-at-one-side-like-a-small-smile-in-a-rounded-square.png',
         id: 7
       },
 
 
       {
-        name: 'Navigate',
-        url: './assets/unt.png',
-
+        name: 'Whitening',
+        url: 'https://png.pngtree.com/svg/20161020/5101007c9d.png',
         id: 8
       },
 
       {
-        name: 'Contact',
-        url: './assets/unt.png',
+        name: 'Blemishe',
+        url: 'https://static.thenounproject.com/png/944433-200.png',
 
         id: 9
       },
       {
-        name: 'ETC',
-        url: './assets/unt.png',
+        name: 'Small Nose',
+        url: 'https://cdn.iconscout.com/icon/premium/png-256-thumb/itchy-nose-777715.png',
         id: 10
       },
     ]
 
-    const url = './assets/unt.png'
 
-    const screenWidth = Math.round(Dimensions.get('window').width);
+
+
+
+    const DURATION = 10000;
+
+
 
     return (
       <Container style={styles.container}>
@@ -131,17 +149,47 @@ export default class App extends Component {
         <Content>
 
           <View>
-            <Card style={{ marginRight: 230, marginLeft: 40, marginTop: 35 }}>
+            <Card style={{ marginRight: 200, marginLeft: 25, marginTop: 10 }}>
               <CardItem>
-                <Text>
-                  User
+                <Text style={{ fontSize: 10, fontWeight: 'bold' }}>
+                  3:00PM - 5:00PM
                 </Text>
+
+              </CardItem>
+              <CardItem style={{ marginTop: -20, backgroundColor: 'transparent' }}>
+                <Text style={{ fontSize: 15, fontWeight: 'bold' }}>
+                  John Key
+                </Text>
+
+              </CardItem>
+
+
+              <CardItem style={{ marginTop: -20, backgroundColor: 'transparent' }}>
+                <Text style={{ fontSize: 10, fontWeight: '500', color: 'lightgrey' }}>
+                  With Judy Brown
+                </Text>
+
               </CardItem>
             </Card>
-            <Card style={{ marginRight: 230, marginLeft: 40, }}>
+
+            <Card style={{ marginRight: 200, marginLeft: 25, marginTop: 5 }}>
               <CardItem>
-                <Text>
-                  Users
+                <Text style={{ fontSize: 10, fontWeight: 'bold' }}>
+                  3:00PM - 5:00PM
+                </Text>
+
+              </CardItem>
+              <CardItem style={{ marginTop: -20, backgroundColor: 'transparent' }}>
+                <Text style={{ fontSize: 15, fontWeight: 'bold' }}>
+                  John Key
+                </Text>
+
+              </CardItem>
+
+
+              <CardItem style={{ marginTop: -20, backgroundColor: 'transparent' }}>
+                <Text style={{ fontSize: 10, fontWeight: '500', color: 'lightgrey' }}>
+                  With Judy Brown
                 </Text>
 
               </CardItem>
@@ -152,10 +200,10 @@ export default class App extends Component {
 
           <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end' }}>
             <WebView
-              style={{ height: 180, marginTop: 5, marginRight: 5, marginLeft: 5 }}
+              style={{ height: 180, marginRight: 5, marginLeft: 5 }}
               javaScriptEnabled={true}
               domStorageEnabled={true}
-              source={{ uri: this.showVideo() }}
+              source={{ uri: this.showVideo(DURATION) }}
             />
 
           </View>
@@ -169,8 +217,9 @@ export default class App extends Component {
 
               {BUTTONS1.map(item => (
                 <Button vertical key={item.id} onPress={() => this.changeId(item.id)}>
-                  <Icon name={item.iconName} />
-                  <Text>{item.name}</Text>
+                  <Image style={{ width: 21, height: 21 }} source={{ uri: item.url }} />
+
+                  <Text style={{ fontSize: 12, marginTop: 8 }}>{item.name}</Text>
                 </Button>
               ))}
 
@@ -184,9 +233,9 @@ export default class App extends Component {
 
               {BUTTONS2.map(item => (
                 <Button vertical key={item.id} onPress={() => this.changeId(item.id)}>
-                  <Image style={{width: 28, height: 28}} source={{uri: item.url}} />
+                  <Image style={{ width: 21, height: 21 }} source={{ uri: item.url }} />
 
-                  <Text>{item.name}</Text>
+                  <Text style={{ fontSize: 12, marginTop: 8 }}>{item.name}</Text>
                 </Button>
               ))}
 
